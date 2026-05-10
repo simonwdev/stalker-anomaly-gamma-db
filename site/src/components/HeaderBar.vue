a9bb51644b105e8f8812847bc6c319a54172d809
Simon Williams <simon@haloconnect.io>
2026-05-10 18:03:50 +0800

    Release notes UX

--- site/src/components/HeaderBar.vue
+++ site/src/components/HeaderBar.vue
@@ -28,7 +28,7 @@
     </div>
 
     <!-- Mobile: overflow menu -->
-    <button class="header-link header-mobile-items" @click="overflowOpen = !overflowOpen">
+    <button class="header-link header-mobile-items" :class="{ 'has-unseen': hasUnseenReleaseNotes }" @click="overflowOpen = !overflowOpen">
         <LucideEllipsisVertical :size="16" />
         <span v-if="hasUnseenReleaseNotes" class="release-notes-badge"></span>
     </button>
@@ -143,7 +143,7 @@
         <button class="header-link shortcut-help-btn" @click="$emit('openShortcutHelp')" v-tooltip="t('app_shortcuts_title')">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="M6 8h4"/><path d="M14 8h4"/><path d="M6 12h3"/><path d="M15 12h3"/><path d="M10 12h4"/><path d="M8 16h8"/></svg>
         </button>
-        <a href="release-notes/" class="release-notes-btn header-link" v-tooltip="t('app_release_notes')">
+        <a href="release-notes/" class="release-notes-btn header-link" :class="{ 'has-unseen': hasUnseenReleaseNotes }" v-tooltip="t('app_release_notes')">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/></svg>
             <span v-if="hasUnseenReleaseNotes" class="release-notes-badge"></span>
         </a>
